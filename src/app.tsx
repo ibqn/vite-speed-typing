df2d70dd28f872c2a688bb44dcc3821cc49bebb7
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-02-10 11:22:16 +0100

    Add generated words and countdown timer components

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,15 @@
+import { faker } from '@faker-js/faker'
+import { useMemo } from 'react'
+import { GeneratedWords } from '@/components/generated-words'
+import { CountdownTimer } from '@/components/countdown-timer'
+
 export const App = () => {
-  return <div className="text-primary-500 text-4xl">hello</div>
+  const words = useMemo(() => faker.word.words(10), [])
+
+  return (
+    <>
+      <CountdownTimer timeLeft={30} />
+      <GeneratedWords words={words} />
+    </>
+  )
 }
