e36b37d5d35927b3527c713c29a10d7f3e08d571
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-02-10 11:43:11 +0100

    Add RestartButton component to App

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker'
 import { useMemo } from 'react'
 import { GeneratedWords } from '@/components/generated-words'
 import { CountdownTimer } from '@/components/countdown-timer'
+import { RestartButton } from '@/components/restart-button'
 
 export const App = () => {
   const words = useMemo(() => faker.word.words(10), [])
@@ -10,6 +11,10 @@ export const App = () => {
     <>
       <CountdownTimer timeLeft={30} />
       <GeneratedWords words={words} />
+      <RestartButton
+        className="mx-auto mt-10 text-slate-500"
+        onRestart={() => null}
+      />
     </>
   )
 }
