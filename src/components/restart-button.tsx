928cca63865b38e048250ed7fed722a3f87b2f0f
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-02-10 11:43:04 +0100

    Add RestartButton component

---
+++ src/components/restart-button.tsx
@@ -0,0 +1,30 @@
+import { ComponentProps, useRef } from 'react'
+import { RotateCw } from 'lucide-react'
+import { cn } from '@/util/class-names'
+
+type Props = {
+  onRestart: () => void
+} & ComponentProps<'button'>
+
+export const RestartButton = ({
+  className,
+  onRestart,
+  ...otherProps
+}: Props) => {
+  const buttonRef = useRef<HTMLButtonElement>(null)
+
+  const handleClick = () => {
+    buttonRef.current?.blur()
+    onRestart()
+  }
+  return (
+    <button
+      ref={buttonRef}
+      onClick={handleClick}
+      {...otherProps}
+      className={cn('block rounded px-8 py-2 hover:bg-slate-700/50', className)}
+    >
+      <RotateCw className="h-6 w-6" />
+    </button>
+  )
+}
