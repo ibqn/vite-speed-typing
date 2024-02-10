b7435e5f64961e4a898e01020236bb378b6f2ac2
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-02-10 15:53:39 +0100

    Add WordsContainer component

---
+++ src/components/words-container.tsx
@@ -0,0 +1,15 @@
+import { cn } from '@/util/class-names'
+import { type ComponentProps } from 'react'
+
+type Props = {} & ComponentProps<'div'>
+
+export const WordsContainer = (props: Props) => (
+  <div
+    className={cn(
+      'relative mt-3 max-w-xl break-all text-4xl leading-relaxed',
+      props.className
+    )}
+  >
+    {props.children}
+  </div>
+)
