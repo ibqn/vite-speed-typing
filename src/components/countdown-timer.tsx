a2b944d316d3f25a8a1c5cca4c7098d315cb49b8
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-02-11 08:51:32 +0100

    Refactor countdown timer component to use timeLeft prop directly

--- src/components/countdown-timer.tsx
+++ src/components/countdown-timer.tsx
@@ -1,19 +1,7 @@
-import { useEffect, useState } from 'react'
-
 type Props = {
   timeLeft: number
 }
 
 export const CountdownTimer = ({ timeLeft }: Props) => {
-  const [time, setTime] = useState(timeLeft)
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime((prev) => prev - 1)
-    }, 1000)
-
-    return () => clearInterval(interval)
-  }, [])
-
-  return <div className="text-primary-400 font-medium">Time: {time}</div>
+  return <div className="text-primary-400 font-medium">Time: {timeLeft}</div>
 }
