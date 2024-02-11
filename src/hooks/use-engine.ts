2c461329008baab0fc98d2a6d4415d919eb037b4
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-02-11 11:29:47 +0100

    Refactor useEngine hook to include error tracking and restart functionality

--- src/hooks/use-engine.ts
+++ src/hooks/use-engine.ts
@@ -1,7 +1,9 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useWords } from '@/hooks/use-words'
 import { useCountdownTimer } from '@/hooks/use-countdown-timer'
 import { useTypings } from '@/hooks/use-typings'
+import { countErrors } from '@/util/count-errors'
+import { log } from '@/util/log'
 
 export type State = 'idle' | 'running' | 'finished'
 
@@ -13,27 +15,68 @@ export const useEngine = () => {
   const { words, updateWords } = useWords(NUMBER_OF_WORDS)
   const { timeLeft, startCountDown, resetCountDown } =
     useCountdownTimer(COUNTDOWN_SECONDS)
-  const { typed, cursor, clearTyped, resetTotalTyped } = useTypings(
+  const { typed, cursor, clearTyped, resetTotalTyped, totalTyped } = useTypings(
     state !== 'finished'
   )
 
-  const start = () => {
-    setState('running')
-    updateWords()
-  }
+  const [errors, setErrors] = useState(0)
 
-  const stop = () => {
-    setState('finished')
-  }
+  const isStarting = useMemo(
+    () => state === 'idle' && cursor > 0,
+    [state, cursor]
+  )
+
+  const areWordsFinished = useMemo(
+    () => words.length === cursor,
+    [words, cursor]
+  )
+
+  const sumErrors = useCallback(() => {
+    const wordsReached = words.substring(0, cursor)
+    setErrors((prevErrors) => prevErrors + countErrors(typed, wordsReached))
+  }, [cursor, typed, words])
+
+  const restart = useCallback(() => {
+    log('restart')
+
+    setState('idle')
+    setErrors(0)
+    resetCountDown()
+    resetTotalTyped()
+    updateWords()
+    clearTyped()
+  }, [clearTyped, resetCountDown, resetTotalTyped, updateWords])
+
+  useEffect(() => {
+    if (isStarting) {
+      setState('running')
+      startCountDown()
+    }
+  }, [isStarting, startCountDown])
+
+  useEffect(() => {
+    if (state === 'running' && timeLeft === 0) {
+      setState('finished')
+      sumErrors()
+    }
+  }, [state, sumErrors, timeLeft])
+
+  useEffect(() => {
+    if (areWordsFinished) {
+      log('words finished')
+      sumErrors()
+      updateWords()
+      clearTyped()
+    }
+  }, [areWordsFinished, sumErrors, updateWords, clearTyped])
 
   return {
     state,
     words,
-    start,
-    stop,
     timeLeft,
-    startCountDown,
-    resetCountDown,
     typed,
+    totalTyped,
+    errors,
+    restart,
   }
 }
